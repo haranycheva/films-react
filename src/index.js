61ad9121f644845e0335a107ccfb6ffb606309f5
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-05-18 22:46:59 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 
+import { GlobalStyled } from 'GlobalStyle.styled';
+import { App } from 'components/App/App';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<BrowserRouter basename='films-react'>
+    <App></App>
+</BrowserRouter>
 );
