c2e951907039a642407e8ea64542af731c2cc411
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-06-10 14:47:37 +0300

    edits

--- src/components/genres/CardsList/CardsList.jsx
+++ src/components/genres/CardsList/CardsList.jsx
@@ -8,18 +8,28 @@ import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const LoaderWrapp = styled.div`
-position: absolute;
-top: 250px;
-left: 50%;
-transform: translateX(-50%);
-`
+  position: absolute;
+  top: 0px;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translateX(-50%);
+  background-color: #fff;
+`;
+
+const StyledLoader = styled.div`
+  position: absolute;
+  top: 250px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
 
 export const List = styled.ul`
-position: relative;
-  padding-top: 40px;
-  padding-left: 50px;
+  position: relative;
+  padding: 40px 30px;
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   gap: 32px;
 `;
 
@@ -37,29 +47,31 @@ export const Item = styled.li`
 
 export function CardsList() {
   const [filmsList, setFilmsList] = useState(null);
-  const [error, setError, isLoading, setLoading] = useErrorAndLoading(false)
+  const [error, setError, isLoading, setLoading] = useErrorAndLoading(false);
   const { genreId } = useParams();
   useEffect(() => {
     const fetch = async () => {
       const data = await getFilmsByGenre(genreId);
       setFilmsList(data);
     };
-    setLoading(true)
-    tryCatchFn(fetch,setLoading, setError);
-    setLoading(false)
+    tryCatchFn(fetch, setLoading, setError);
   }, [genreId, setError, setLoading]);
   return (
     <>
       {error && <p>Ooooooooooops.... Something went wrong.....</p>}
-        <List>
-        {isLoading && <LoaderWrapp><Loader  /></LoaderWrapp>}
-        {filmsList?.length > 0 && (
+      <List>
+        { isLoading &&
+          <LoaderWrapp>
+            <StyledLoader><Loader/></StyledLoader>
+          </LoaderWrapp>
+        }
+        {filmsList?.length > 0 &&
           filmsList.map((film) => (
             <Item key={film.id}>
               <FilmCard film={film} />
             </Item>
-          )))}
-        </List>
+          ))}
+      </List>
     </>
-    )
+  );
 }
